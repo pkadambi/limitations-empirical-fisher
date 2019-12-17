bb3e458fb49b2ccb82a05cc8832486337f629bcf
pkadambi <pkadambi1@gmail.com>
2019-12-16 23:18:26 -0700

    fixed manual BCELoss (correctly adding EPSILON before log)

--- torchplots/logistic_model.py
+++ torchplots/logistic_model.py
@@ -2,44 +2,216 @@ import torch
 import torch.nn as nn
 import tensorflow as tf
 import numpy as np
+import torch.nn.functional as F
+
+def jacobian(y, x, create_graph=False):
+    jac = []
+    flat_y = y.reshape(-1)
+    grad_y = torch.zeros_like(flat_y)
+    for i in range(len(flat_y)):
+        grad_y[i] = 1.
+        grad_x, = torch.autograd.grad(flat_y, x, grad_y, retain_graph=True, create_graph=create_graph)
+        jac.append(grad_x.reshape(x.shape))
+        grad_y[i] = 0.
+    return torch.stack(jac).reshape(y.shape + x.shape)
 
 
-class Logistic():
+def hessian(y, x):
+    return jacobian(jacobian(y, x, create_graph=True), x)
 
-    def __init__(self, X, y, n_classes, n_features, loss='logistic'):
+def wellspecified_logreg(N):
 
-        self.X = X
-        self.y = y
-        self.N = X.shape[0]
+    mean1 = np.array([1, 1]).reshape((-1, 1))
+    mean2 = np.array([-1, -1]).reshape((-1, 1))
+    cov1 = np.identity(2) * 2
+    cov2 = np.identity(2) * 2
+
+    X1 = mean1.T + np.matmul(np.random.randn(int(N / 2), 2), cov1)
+    X2 = mean2.T + np.matmul(np.random.randn(int(N / 2), 2), cov2)
+    y1 = np.zeros((int(N / 2), 1))
+    y2 = np.ones((int(N / 2), 1))
+
+    X = np.vstack([X1, X2])
+    y = np.vstack([y1, y2])
+    return X, y
+
+def binary_cross_entropy(yhat, y):
+    # loss = -(yhat.log() * y + (1 - yhat).log() * (1 - y))
+    EPS=1e-8
+    yh = yhat.clone()
+    yh[yh >(1-EPS)]=1-EPS
+    yh[yh <EPS] = EPS
+
+    loss = -(yh.log() * y + (1 - yh).log() * (1 - y))
+    # loss = -((yhat + 1e-10).log() * y + (1 - yhat + 1e-10).log() * (1 - y))
+    return loss.mean()
+
+
+class LogisticModel:
+
+    def __init__(self, N = 1000, loss='logistic'):
+        np.random.seed(0)
+
+        X, y = wellspecified_logreg(N)
+
+        self.X = torch.tensor(X)
+        self.y = torch.tensor(y).double()
+        # print(self.y)
+        # exit()
+        self.N = N
         self.D = X.shape[1]
 
-        self.theta = torch.tensor(np.random.randn(n_classes, n_features), requires_grad = True)
-        self.b = torch.tensor(np.random.randn(n_classes))
-        if loss=='logistic':
-            self.criterion = nn.NLLLoss()
+        self.theta = torch.tensor([5., 5.], requires_grad = True)
 
-        #TODO: implement squared loss
-        # elif loss=='squared':
-        #     pass
 
-        else:
-            exit('ERROR: Choose correct loss function')
+    def set_theta(self, theta):
+        self.theta = torch.tensor(theta, requires_grad = True)
+        # self.b = torch.tensor(theta[1], requires_grad = True)
+        # self.theta_vec = torch.cat((self.theta, self.b), 0)
 
     def compute_loss(self, y_hat):
-        
+        '''
 
+        :param y_hat:
+        :return:
+        '''
+
+        return binary_cross_entropy(y_hat, self.y)
 
     def forward(self):
 
-        pass
+        # Data
+        n_examples_per_class = 500
+
+        # Forward Function
+        # x_vec = torch.cat((self.X, torch.ones_like(x)), 1)
+
+        z = self.X @ self.theta
+        yhat = torch.sigmoid(z)
+        # print(yhat)
+        return yhat
+
+        # Loss
+
+
+    def compute_GD_gradient(self, theta, retain_original_theta=False):
+
+        #Zero out gradients
+        if self.theta.grad is not None:
+            self.theta.grad=None
+
+        if retain_original_theta:
+            theta_orig = self.theta.clone()
+
+        self.set_theta(theta)
+
+        yhat = self.forward()
+        lossval = self.compute_loss(y_hat=yhat)
+        # print(lossval)
+        lossval.backward()
+
+        if retain_original_theta:
+            self.theta = theta_orig.clone()
+
+        return self.theta.grad.data.numpy()
+
+
+    def compute_NGD_gradient(self, theta, retain_original_theta=False, gamma=1/12):
+
+        #Zero out gradients
+        if self.theta.grad is not None:
+            self.theta.grad=None
+
+        if retain_original_theta:
+            theta_orig = self.theta.clone()
+
+        self.set_theta(theta)
+
+        yhat = self.forward()
+        lossval = self.compute_loss(y_hat=yhat)
+
+        # compute gradient (used for solving linear system for NGD update)
+        lossval.backward(retain_graph=True)
+        g = self.theta.grad.data.detach().numpy()
+
+        # if self.theta.grad is not None:
+        #     self.theta.grad=None
+
+        hess = hessian(lossval, self.theta) / self.N
+        # print('hess')
+        hess_data = np.squeeze(hess.numpy()) * self.N
+        # print('hess_')
+
+
+        # In accordance with EFTK
+        # hess_prior = 10.*np.eye(self.D)/self.N
+        hess = hess_data
+
+        ngd_update = .5 * 1/8 * np.linalg.solve(hess + (1e-8), g)
+        # print(torch.squeeze(yhat))
+        # print('Theta:')
+        # print(theta)
+        # print('Hessian:')
+        # print(hess)
+        # print('G')
+        # print(g)
+        # print('NGD Update:')
+        # print(ngd_update)
+        # print('hess data:')
+        # print(hess_data)
+        # print('loss')
+        # print(lossval)
+        # exit()
+
+        if retain_original_theta:
+            self.theta = theta_orig.clone()
+
+        return ngd_update
+
+    def compute_EF_gradient(self, theta, retain_original_theta=False):
+
+        #Zero out gradients
+        if self.theta.grad is not None:
+            self.theta.grad=None
+
+        if retain_original_theta:
+            theta_orig = self.theta.clone()
+
+        self.set_theta(theta)
+
+        yhat = self.forward()
+        lossval = self.compute_loss(y_hat=yhat)
+        # print(lossval)
+        lossval.backward()
+
+        if retain_original_theta:
+            self.theta = theta_orig.clone()
+
+        return self.theta.grad.data.detach().numpy()
 
     def update_theta(self, lr):
 
         pass
 
-    def loss(self):
+    def loss_value(self, theta, retain_original_theta=False):
+        '''
 
-        return self.criterion()
+        For a specific value of theta, compute the loss.
+        Note that it will update the value of theta with what's provided here
+
+        :param theta: 2x1 torch tensor
+        :return:
+        '''
+        if retain_original_theta:
+            theta_orig = self.theta.clone()
+
+        self.set_theta(theta)
+        yhat = self.forward()
+        # print(yhat)
+        if retain_original_theta:
+            self.theta = theta_orig.clone()
+
+        return self.compute_loss(y_hat=yhat)
 
     def grad(self):
 
@@ -52,12 +224,12 @@ class Logistic():
     def ef(self):
         pass
 
-    def hess_data(self, theta):
-        theta_ = theta.reshape((-1, 1))
-        J_theta_f = self.X.T
-        f = np.matmul(self.X, theta_).reshape((-1, 1))
-        H_f_loss_diag = (h.sigmoid(f) * (1 - h.sigmoid(f))).reshape((-1, 1))
-        return np.matmul(J_theta_f * H_f_loss_diag.T, J_theta_f.T) / self.N
+    # def hess_data(self, theta):
+    #     theta_ = theta.reshape((-1, 1))
+    #     J_theta_f = self.X.T
+    #     f = np.matmul(self.X, theta_).reshape((-1, 1))
+    #     H_f_loss_diag = (h.sigmoid(f) * (1 - h.sigmoid(f))).reshape((-1, 1))
+    #     return np.matmul(J_theta_f * H_f_loss_diag.T, J_theta_f.T) / self.N
 
 
 
@@ -71,26 +243,26 @@ class Softmax():
         pass
 
 
-class LogisticQuantized():
-    def __init__(self, is_quantized=False):
-        self.theta =
-        self.is_quantized = is_quantized
-
-
-        if quantizer is None:
-            #TODO: set up the quantizer here
-
-    def forward(self, data, is_quantized):
-
-    def grad(self):
-
-
-
-    def hess(self):
-
-
-
-    def ef(self):
+# class LogisticQuantized():
+#     def __init__(self, is_quantized=False):
+#         self.theta =
+#         self.is_quantized = is_quantized
+#
+#
+#         if quantizer is None:
+#             #TODO: set up the quantizer here
+#
+#     def forward(self, data, is_quantized):
+#
+#     def grad(self):
+#
+#
+#
+#     def hess(self):
+#
+#
+#
+#     def ef(self):
 
 
 
--- torchplots/logregtester.py
+++ torchplots/logregtester.py
@@ -1,10 +1,104 @@
 import numpy as np
 import torch.nn as nn
 import torch
+import matplotlib.pyplot as plt
+import matplotlib
 import torch.nn.functional as F
 import torch.autograd as autograd
 from torch.autograd import grad
+from numpy import inf
+from logistic_model import LogisticModel
+import pdb
 
+GRID_DENS = 15
+LEVELS = 5
+LABELPAD_DIFF = -20
+
+optNames = ["GD", "NGD", "EF"]
+# label_for = [
+#     r"\bf{Dataset}",
+#     r"\bf{GD}",
+#     r"\bf{NGD}",
+#     r"\bf{EF}"
+# ]
+
+# optNames = ["MSQE", "Full Hessian", "EF"]
+label_for = [
+    r"\bf{Dataset}",
+    r"\bf{MSQE}",
+    r"\bf{Full Hessian}",
+    r"\bf{EF}"
+]
+
+
+DD = 3
+theta_lims = [-.5 - DD, -.5 + DD]
+thetas = list([np.linspace(theta_lims[0], theta_lims[1], GRID_DENS) for _ in range(2)])
+
+# print(thetas)
+# print(theta_lims)
+# exit()
+
+class Quantizer:
+
+    def __init__(self, num_bits, q_min, q_max):
+
+        self.qmin = q_min
+        self.qmax = q_max
+        self.n_bits = num_bits
+
+
+        if self.n_bits<4:
+            self.bins = np.linspace(self.qmin, self.qmax, 2 ** self.n_bits)
+        else:
+            self.bins = np.linspace(self.qmin, self.qmax, 2 ** 4)
+
+    def quantize(self, x):
+
+        if self.n_bits<32:
+            xq = np.copy(x)
+
+            n_intervals = 2 ** self.n_bits -1
+
+            qrange = self.qmax - self.qmin
+            scale = n_intervals / qrange
+
+
+            #The following rescales x to the range between [0,1,2,..., 2^(n_bits)-1]
+            xq = (xq - self.qmin) * scale
+
+            #Quantize to [0,1,2,..., 2^(n_bits)-1]
+            xq = np.clip(xq, 0, n_intervals)
+
+            xq = np.round(xq)
+
+            #Undo scaling
+            xq = xq / scale + self.qmin
+
+            return xq
+
+        else:
+            return x
+
+def hide_ticks(axes):
+    for ax in axes:
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+
+def hide_labels(axes):
+    for ax in axes:
+        ax.set_xlabel("")
+        ax.set_ylabel("")
+
+
+def save(fig, name):
+    fig.savefig(name, bbox_inches='tight', transparent=True)
+
+
+def strip_axes(axes):
+    hide_ticks(axes)
+    hide_labels(axes)
 
 def jacobian(y, x, create_graph=False):
     jac = []
@@ -25,7 +119,6 @@ def hessian(y, x):
 def binary_cross_entropy(x, y):
     loss = -(x.log() * y + (1 - x).log() * (1 - y))
     return loss.mean()
-
 
 
 # Function defenitions
@@ -57,8 +150,11 @@ yhat = sfmx(z)
 
 # Loss
 loss = binary_cross_entropy(yhat, y)
+# loss = criterion(yhat, y)
+print(yhat)
+print(y)
 print(loss)
-
+# exit()
 print('\nGRADIENTS')
 loss.backward(retain_graph=True)
 print(theta.grad)
@@ -80,39 +176,147 @@ for i in range(10000):
     print(i)
     print(jacobian(f, ab))
     print(hessian(f, ab))
+    print('Done printing hessian')
+    break
+# exit()
 
+def plot_loss_contour(ax, problem):
 
-def plot_vecFields(axes, problem, vecFuncs, with_loss_contour=True):
+    def compute_losses(lossFunc):
 
-    def vectorField(vecFunc):
-        vector_field = [np.zeros((GRID_DENS, GRID_DENS)), np.zeros((GRID_DENS, GRID_DENS))]
+        losses = np.zeros((len(thetas[0]), len(thetas[1])))
+
         for i, t1 in enumerate(thetas[0]):
             for j, t2 in enumerate(thetas[1]):
                 theta = np.array([t1, t2]).reshape(-1, 1)
-                v = vecFunc(theta)
+                # losses[i, j] = problem.loss_value(theta)
+                losses[i, j] = lossFunc(theta)
+                # print(theta)
+                # print(losses[i, j])
+                # print('asdf')
+                # exit()
+        return losses
+
+    losses = compute_losses(lambda t: problem.loss_value(t))
+    # print(thetas[0])
+    # print(thetas[1])
+    losses = np.array(losses)
+    # print(losses)
+    losses[losses==inf]=.0001
+    losses[losses==.0001]=np.max(losses)
+
+    # print(losses)
+    # print(LEVELS)
+    # exit()
+    ax.contour(thetas[0], thetas[1], losses.T, LEVELS, colors=["k"], alpha=0.3)
+
+def plot_vecFields(axis, problem):
+
+    def vectorField(problem, optimization_method = 'NGD'):
+        '''
+        :param problem: an instance of the problem
+        :param optimization_method: Either 'GD', 'NGD', 'EF', or 'DISTIL'
+        :return: The vector field to be plotted
+        '''
+        vector_field = [np.zeros((GRID_DENS, GRID_DENS)), np.zeros((GRID_DENS, GRID_DENS))]
+
+        if optimization_method is 'GD':
+            gradFunc = problem.compute_GD_gradient
+
+        elif optimization_method is 'NGD':
+            gradFunc = problem.compute_NGD_gradient
+
+        elif optimization_method is 'EF':
+            gradFunc = problem.compute_EF_gradient
+
+        elif optimization_method is 'DISTIL':
+            gradFunc = problem.compute_DISTIL_gradient
+
+        for i, t1 in enumerate(thetas[0]):
+            for j, t2 in enumerate(thetas[1]):
+
+                theta = np.array([t1, t2]).reshape(-1, 1)
+                v = gradFunc(theta)
+                # pdb.set_trace()
+
+                # v[v>2]=2
+
+                v = -np.squeeze(v)
+
+                # print( np.squeeze(v))
+                # print(np.shape(vector_field))
+
                 for d in range(2):
-                    # import pdb
-                    # pdb.set_trace()
-                    # print(vecFunc)
-                    # print(theta)
-                    # print(d)
-                    # print(vector_field[d][i, j])
-                    # print(v[d])
                     vector_field[d][i, j] = v[d]
-                    vector_field[d][i, j] = v[d]
+
+
+        vector_field = np.array(vector_field)
+        # vector_field[vector_field>2.]=2.
+        # print(vector_field)
+        # exit()
         return vector_field
 
     def plot_vecField(ax, vecField, scale=30):
         U = vecField[0].copy().T
         V = vecField[1].copy().T
-        cf = ax.quiver(thetas[0], thetas[1], U, V, angles='xy', scale=scale, color=efplt.grays["dark"], width=0.005, headwidth=4, headlength=3)
+        cf = ax.quiver(thetas[0], thetas[1], U, V, angles='xy', scale=scale, color=[.6, .6, .6], width=0.0025, headwidth=4, headlength=3)
         return cf
 
-    vecFields = list([vectorField(vecFuncs[i]) for i in range(len(axes))])
-    for ax, vecField in zip(axes, vecFields):
-        if with_loss_contour:
-            plot_loss_contour(ax, problem)
-        plot_vecField(ax, vecField)
+    plot_loss_contour(axis, problem)
+
+    #TODO: uncomment the 2 lines below after you have figured out how to get the loss contours plotted with torch
+    vecField = vectorField(problem)
+    plot_vecField(axis, vecField)
+
+def fig_and_axes():
+    fig = plt.figure()
+    axis = plt.gca()
+
+    # Below for single plot
+    # fig = plt.figure(figsize=(12, 3.2))
+    # gs = matplotlib.gridspec.GridSpec(1, 1)
+    # gs.update(left=0.02, right=1, wspace=0.1, hspace=0.2, bottom=0.05, top=0.90)
+
+    # axes = fig.add_subplot(gs[0,0])
+    # return fig, axes
+
+    return fig, axis
+
+
+fig, axis = fig_and_axes()
+# print(fig)
+# print(axis)
+# exit()
+
+axis.set_xlim(theta_lims)
+axis.set_ylim(theta_lims)
+
+lr_problem = LogisticModel()
+plot_vecFields(axis, lr_problem)
+
+
+axis.set_xlabel('x', labelpad=LABELPAD_DIFF)
+axis.set_ylabel('y', labelpad=LABELPAD_DIFF)
+
+axis.set_xlabel(r"$\theta_1$", labelpad=LABELPAD_DIFF)
+axis.set_ylabel(r"$\theta_2$", labelpad=LABELPAD_DIFF)
+
+q = Quantizer(num_bits=3, q_min=-3, q_max=2.5)
+
+axis.set_xticks(q.bins)
+axis.set_yticks(q.bins)
+axis.grid(which='major', alpha=1., linewidth=2, color='k')
+
+plt.show()
+
+
+
+startingPoints = [
+    np.array([-.5, 2]).reshape((-1, 1)),
+    np.array([-1.5, -2.5]).reshape((-1, 1)),
+    np.array([2, .5]).reshape((-1, 1)),
+    np.array([-3, .5]).reshape((-1, 1)),
+]
 
 
 
